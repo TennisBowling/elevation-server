80ed6baa70e95e0cb25834dab26522615ab1b7d7
TennisBowling <tennisbowling@tennisbowling.com>
2024-10-20 01:07:59 -0700

    change to websocket

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::net::SocketAddr;
 use std::sync::Arc;
-use axum::extract::State;
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::{routing, Router};
-use axum_macros::debug_handler;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+use futures_util::{SinkExt, StreamExt};
 use gdal::Dataset;
 use gdal::raster::RasterBand;
 use tracing_subscriber::EnvFilter;
-
 
 struct ElevationData {
     _dataset: Dataset, // Keep dataset alive
@@ -24,43 +22,44 @@ fn get_elev(band: &RasterBand, transform: &[f64; 6], lat: &f64, lon: &f64) -> f6
     let col = ((lon - transform[0]) / transform[1]) as isize;
     let row = ((lat - transform[3]) / transform[5]) as isize;
 
-    let buf = match band.read_as::<f64>(
+    let buf = band.read_as::<f64>(
         (col, row),
         (1, 1),
         (1, 1),
         Some(gdal::raster::ResampleAlg::CubicSpline),
-    ) {
-        Ok(buf) => buf,
-        Err(_) => {
-            return f32::MIN as f64;
-        }
-    };
+    ).unwrap();
     buf.data()[0]
 }
 
-#[debug_handler(state = (Arc<ElevationData>, Arc<[f64; 6]>))]
-async fn get_elev_route(
-    State((elev_data, transform)): State<(Arc<ElevationData>, Arc<[f64; 6]>)>,
-    body: String,
-) -> impl IntoResponse {
+async fn handle_connection(
+    stream: TcpStream,
+    elev_data: Arc<ElevationData>,
+    transform: Arc<[f64; 6]>,
+) {
+    let ws_stream = accept_async(stream).await.unwrap();
+    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+
+    while let Some(msg) = ws_receiver.next().await {
+        let msg = msg.unwrap();
+
+        let mut latlon = msg.to_text().unwrap().split(',');
+        let lat = latlon.next().unwrap().parse::<i32>().unwrap();
+        let lon = latlon.next().unwrap().parse::<i32>().unwrap();
 
 
-    let mut latlon = body.split(',');
-    let lat = latlon.next().unwrap().parse::<i32>().unwrap();
-    let lon = latlon.next().unwrap().parse::<i32>().unwrap();
+        let elev = get_elev(
+            &elev_data.rasterband,
+            &transform,
+            &(lat as f64 * 1e-7),
+            &(lon as f64 * 1e-7),
+        ) as f32;
 
-    let elev = get_elev(
-        &elev_data.rasterband,
-        &transform,
-        &(lat as f64 * 1e-7),
-        &(lon as f64 * 1e-7),
-    ) as f32;
+        if elev == f32::MIN {
+            tracing::warn!("Elevation not found");
+        }
 
-    if elev == f32::MIN {
-        tracing::warn!("Couldn't find elevation");
+        ws_sender.send(Message::Text(elev.to_string())).await.unwrap();
     }
-
-    (StatusCode::OK, elev.to_string())
 }
 
 #[tokio::main]
@@ -91,27 +90,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dataset = Dataset::open(location).unwrap();
     tracing::info!("Opened elevation dataset");
 
-
-
     let elev_data = Arc::new(ElevationData {
-        rasterband: unsafe { std::mem::transmute(dataset.rasterband(1).unwrap()) },     // We have to get the rasterband as 'static. We KNOW it'll exist for the lifetime of the server
+        rasterband: unsafe { std::mem::transmute(dataset.rasterband(1).unwrap()) }, // We have to get the rasterband as 'static. We KNOW it'll exist for the lifetime of the server
         _dataset: dataset,
     });
 
-    let elev_transform: Arc<[f64; 6]> = Arc::new(elev_data._dataset.geo_transform().expect("Failed to get geotransform"));
-
-    let app = Router::new()
-        .route("/", routing::post(get_elev_route))
-        .with_state((elev_data, elev_transform));
-
-
+    let elev_transform: Arc<[f64; 6]> = Arc::new(elev_data._dataset.geo_transform().unwrap());
 
 
     let addr = "0.0.0.0:8083".to_string();
     let addr: SocketAddr = addr.parse().unwrap();
-    let listener = tokio::net::TcpListener::bind(addr).await?;
-    tracing::info!("Listening on {}", addr);
-    
-    axum::serve(listener, app).await?;
+    let listener = TcpListener::bind(&addr).await?;
+    tracing::info!("Websocket server listening on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let elev_data = Arc::clone(&elev_data);
+        let transform = Arc::clone(&elev_transform);
+
+        tokio::spawn(async move {
+            handle_connection(stream, elev_data, transform).await;
+        });
+    }
+
     Ok(())
 }
